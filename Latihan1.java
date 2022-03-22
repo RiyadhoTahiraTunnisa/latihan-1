0090094303a27283be7c09d1f548849a06e9f93e
RiyadhoTahiraTunnisa <102140640+RiyadhoTahiraTunnisa@users.noreply.github.com>
2022-03-22 14:33:59 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author Riyadho Tahira Tunnisa
+ * Date 22 March 2022 12:48 PM
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hallo World");
+    }
+    
+}
